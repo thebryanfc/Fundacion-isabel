707a12f98f5bbcfadabad59465fc83f2ba0e8410
Bryan <bryanyazielfigueroacastillo@gmail.com>
2025-07-24 09:26:26 -0600

    v1.0.0 index

--- resources/views/pages/inicio.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Inicio')
-
-@section('content')
-    <h1 class="mb-4">Bienvenido a la Fundación Isabel</h1>
-    <p>Esta es la página de inicio del sitio web oficial de la fundación.</p>
-@endsection
