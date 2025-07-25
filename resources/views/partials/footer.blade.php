b9a940b26dff6f9cc86c563310c4879b1c68f9b4
Bryan <bryanyazielfigueroacastillo@gmail.com>
2025-07-25 09:28:08 -0600

    footer v1.0.0

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,64 @@
-<footer class="bg-dark text-white text-center py-3 mt-5">
-    <div class="container">
-        &copy; {{ date('Y') }} Fundación Isabel. Todos los derechos reservados.
+<footer style="background-color: #8d3e60ff;" class="text-white text-center py-4 mt-5">
+  <div class="container">
+    <div class="row text-start justify-content-center">
+      
+      <!-- Columna 1: Logo y mensaje -->
+      <div class="col-md-4 mb-4">
+        <img src="https://www.fundacionisabelsoluciones.com/wp-content/uploads/2025/02/cropped-LOGO-DE-FUNDACION-ISABEL-144x144.png" 
+             alt="Logo Fundación Isabel" class="img-fluid mb-3" style="max-width: 100px;">
+        <p>Si necesitas apoyo o más información, no dudes en contactarnos. Juntos podemos generar un cambio positivo.</p>
+      </div>
+
+      <!-- Columna 2: Servicios -->
+      <div class="col-md-4 mb-4">
+        <h5>¿Qué hacemos?</h5>
+        <ul class="list-unstyled">
+          <li>-Promoción a los derechos humanos de las mujeres</li>
+          <hr class="bg-light">
+          <li>-Atención en salud mental</li>
+          <hr class="bg-light">
+          <li>-Construimos liderazgo feminista</li>
+          <hr class="bg-light">
+          <li>-Programas educativos</li>
+        </ul>
+      </div>
+
+      <!-- Columna 3: Imagen y redes sociales -->
+      <div class="col-md-4 mb-4 text-center">
+        <img src="https://www.fundacionisabelsoluciones.com/wp-content/uploads/2025/02/violencia-fundacion-isable-3-1024x576.jpg" 
+             alt="Imagen de la Fundación Isabel" class="img-fluid rounded mb-3 shadow">
+        
+        <h5>Síguenos</h5>
+        <!-- Íconos de redes sociales con imágenes -->
+        <a href="https://www.facebook.com/people/Isabel-Fundacion/100090372888634" target="_blank" class="me-2 text-white">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/600px-2023_Facebook_icon.svg.png" 
+               alt="Facebook" style="width: 24px; height: 24px;">
+        </a>
+        <a href="https://www.instagram.com/fundacionisabel/" target="_blank" class="text-white">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/960px-Instagram_icon.png" 
+               alt="Instagram" style="width: 24px; height: 24px;">
+        </a>
+      </div>
     </div>
+
+    <!-- Nueva sección de texto informativo -->
+    <div class="row text-start mt-4">
+      <div class="col-md-10">
+        <p class="text-start">
+          Somos una organización registrada el 19 de mayo del 2023.
+          <br><br>
+          En Fundación Isabel, nuestro objetivo es brindar apoyo y esperanza a quienes más lo necesitan. Nos enfocamos en empoderar a mujeres y familias a través de programas de educación, salud y desarrollo integral, promoviendo la igualdad de oportunidades y una vida libre de violencia.
+          <br><br>
+          Creemos en el poder de la solidaridad y el compromiso social para transformar vidas. ¡Únete a nuestra misión y sé parte del cambio!
+          <br><br>
+          Todas las donaciones que recibimos son destinadas directamente a brindar apoyo a mujeres en situaciones de calle, violencia y vulnerabilidad. A través de programas de refugio, atención psicológica y empoderamiento, trabajamos para ofrecerles una oportunidad de reconstruir sus vidas con dignidad y esperanza.
+          <br><br>
+          <strong>¡Tu generosidad marca la diferencia!</strong>
+        </p>
+      </div>
+    </div>
+
+    <hr class="bg-light">
+    <p class="mb-0">&copy; 2025 Fundación Isabel. Todos los derechos reservados.</p>
+  </div>
 </footer>
